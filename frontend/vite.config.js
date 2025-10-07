e9af5226a615a3d3fedb6e7523a7fe6d56892b74
shaikhhabib001 <shaikhhabib619@gmail.com>
2025-10-08 02:12:07 +0500

    Real-time data syncronization after deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://smarthealthmonitoringtool-cftt.vercel.app',
         changeOrigin: true,
       },
     },
